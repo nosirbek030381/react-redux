c8943b6b593a1913b2139d1dfe69e7e240843448
nosirbek030381 <ismoilovn899@gmail.com>
2023-11-14 17:08:46 +0500

    #7. Fullstack loyihaga kirish!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Login, Main, Navbar, Register } from './components';
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
-			<Router>
-				<Route path='/login' component={Login} />
-				<Route path='/register' component={Register} />
-				<Route path='/main' component={Main} />
-			</Router>
+			<div className='container'>
+				<Routes>
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/main' element={<Main />} />
+				</Routes>
+			</div>
 		</div>
 	);
 };
